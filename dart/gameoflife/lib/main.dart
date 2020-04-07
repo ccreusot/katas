87a2c4c8e5f8aae5a6121689e7905b2b66041644
Cedric Creusot <cedric.creusot@gmail.com>
2020-04-07 18:33:13 +0200

    Add interface for the game of life

--- dart/gameoflife/lib/main.dart
+++ dart/gameoflife/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:gameoflife/board.dart';
+
+import 'game.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,105 +11,199 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        title: 'Flutter Game Of Life',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage());
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int width = 1;
+  int height = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.only(left: 16, right: 16),
+          color: Colors.lightBlueAccent,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Width",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              ValueWidget(size: width, onAdd: _addWidth, onSub: _subWidth),
+              Text(
+                "Height",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
+              ),
+              ValueWidget(size: height, onAdd: _addHeight, onSub: _subHeight),
+              MaterialButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              GamePage(width: width, height: height)));
+                },
+                child: Text("Start"),
+                color: Colors.greenAccent,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(16))),
+              ),
+            ],
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+
+  void _addWidth() {
+    setState(() {
+      width++;
+    });
+  }
+
+  void _subWidth() {
+    setState(() {
+      if (width > 1) {
+        width--;
+      }
+    });
+  }
+
+  void _addHeight() {
+    setState(() {
+      height++;
+    });
+  }
+
+  void _subHeight() {
+    setState(() {
+      if (height > 1) {
+        height--;
+      }
+    });
+  }
+}
+
+class ValueWidget extends StatelessWidget {
+  final void Function() onAdd;
+  final void Function() onSub;
+  final int size;
+
+  ValueWidget({this.size, this.onAdd, this.onSub});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        IconButton(
+          onPressed: onAdd,
+          icon: Icon(Icons.exposure_plus_1),
+        ),
+        Expanded(
+            child: Center(
+                child: Text("$size",
+                    style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 18)))),
+        IconButton(
+          onPressed: onSub,
+          icon: Icon(Icons.exposure_neg_1),
+        )
+      ],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+class GamePage extends StatefulWidget {
+  final int width;
+  final int height;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  GamePage({this.width, this.height});
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _GamePageState createState() => _GamePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _GamePageState extends State<GamePage>
+    with SingleTickerProviderStateMixin {
+  Game game;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Ticker _ticker;
+
+  @override
+  void initState() {
+    super.initState();
+    game = Game(board: Board.randomized(widget.width, widget.height));
+    _ticker = createTicker(update);
+    _ticker.start();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return Container(
+      color: Colors.lightBlueAccent,
+      child: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            for (int y = 0; y < widget.height; y++)
+              Expanded(
+                child: Row(
+                  children: <Widget>[
+                    for (int x = 0; x < widget.width; x++)
+                      Expanded(
+                          child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Container(
+                          color: game.board.cells[y][x].alive
+                              ? Colors.greenAccent
+                              : Colors.redAccent,
+                        ),
+                      ))
+                  ],
+                ),
+              )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void update(Duration elapsed) {
+    if (elapsed.inMilliseconds >= 500) {
+      setState(() {
+        game.update();
+        if (game.isEndReached()) {
+          _ticker.stop();
+        }
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _ticker.stop();
+    super.dispose();
   }
 }
